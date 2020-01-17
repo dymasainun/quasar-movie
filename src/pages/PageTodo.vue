b346db01d195db47e47bfc758f98143afef9b329
dymasainun <nundymas@gmail.com>
2020-01-17 23:55:28 +0700

    first commit

---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-list 
+      separator
+      bordered>
+    
+      <q-item 
+      v-for="task in tasks"
+      :key="task.id"
+      @click="tack.completed = !task.completed"
+      :class="!task.completed ? 'bg-orange-1' : 'bg-green-1'"
+      clickable
+      v-ripple>
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label
+            :class="{ 'text-strikethrough' : task.completed }">
+            {{ task.name }}
+            </q-item-label>          
+        </q-item-section>
+
+        <q-item-section side>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon 
+                name="event" 
+                size="18px"
+                class="q-mr-xs"/>
+            </div>
+            <div class="column">
+              <q-item-label                 
+                class="row justify-end"
+                caption>
+                {{ task.dueDate }}
+              </q-item-label{{>
+              <q-item-label                 
+                class="row justify-end"
+                caption>
+                <small>{{ task.dueTime }}</small>
+              </q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        tasks: [
+          {
+            id: 1,
+            name: 'Go to shop',
+            compleceted: false,
+            dueDate: '2019/05/12',
+            dueTime: '18.30'
+          },
+          {
+            id: 2,
+            name: 'Go to bananas',
+            compleceted: false,
+            dueDate: '2019/05/12',
+            dueTime: ''
+          },
+          {
+            id: 3,
+            name: 'Go to apples',
+            compleceted: false,
+            dueDate: '2019/05/12',
+            dueTime: ''
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
