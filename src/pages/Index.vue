9697aa37a9a45befdf67b8e2badea729506c3feb
dymasainun <nundymas@gmail.com>
2020-01-23 03:25:07 +0700

    change 3

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  created () {
+    this.$axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=c0771d02')
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
---
+++ src/router/routes.js
@@ -0,0 +1,27 @@
+
+const routes = [
+  {
+    path: '/movie',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Movie.vue') }
+    ]
+  },
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
