1a8ea24cacc89dc4fb8fdfbe55dfc836154dab6f
dymasainun <nundymas@gmail.com>
2020-01-23 03:48:52 +0700

    change 3

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,14 +2,14 @@
 const routes = [
   {
     path: '/movie',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/Layout.vue'),
     children: [
       { path: '', component: () => import('pages/Movie.vue') }
     ]
   },
   {
     path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/Layout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
     ]
