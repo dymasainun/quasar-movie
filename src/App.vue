1672433323e20866943c5c92e88c432f6639f0e5
dymasainun <nundymas@gmail.com>
2020-01-23 17:43:16 +0700

    change4

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,3 @@ export default {
   name: 'App'
 }
 </script>
-<<<<<<< HEAD
-=======
-
-<style>
-  .text-strikethrough {
-    text-decoration: line-through;
-  }
-</style>
->>>>>>> 9e3ffff01b9ce424551a4e6a23ed62c6eba11fa7
